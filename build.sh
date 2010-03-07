00216ba2574384fe2772574963b7ab9c7238a4bd
doug.muth <doug.muth@de3767ec-2675-11df-8747-af52ddc49692>
2010-03-07 01:53:48 +0000

    Modified script to copy everything to a named directory in a temp directory and then tar up the named directory.  This way we can get everything in a directory called "ddt", which is important for Drupal.
    git-svn-id: https://dougs-drupal-tools.googlecode.com/svn/trunk@17 de3767ec-2675-11df-8747-af52ddc49692

--- build.sh
+++ build.sh
@@ -17,21 +17,40 @@ set -e
 # Grab our highest version, so we can get a uniquely named file.
 #
 echo "Checking version...  Make sure you have svn set up!"
-#VERSION=`svn stat -v |cut -c20-26 |sort -r |head -n1 |sed -e s/" "//g`
 VERSION=`git svn info |grep "Revision" |cut -d: -f2 |sed -e s/[^0-9]//`
 
-PWD=`pwd`
-DIR=`basename $PWD`
-TARBALL=${DIR}/ddt-build_${VERSION}.tgz
+#
+# Make a temporary directory
+#
+TMPDIR=`mktemp -d`
+mkdir $TMPDIR/ddt
+
+#
+# Copy everything to our temp directory and go to the parent.
+# The whole "ddt" directory is what the name of the module directory will 
+# be in Drupal
+#
+cp -r * $TMPDIR/ddt
+pushd $TMPDIR
+
+TARBALL=ddt-build_${VERSION}.tgz
 
 #
 # We don't want any revision control files included in the atrball.
 #
 OPTIONS='--exclude RCS --exclude .svn --exclude .git'
 
-cd ..
+#
+# Make our tarball
+#
+tar cfzv ${TARBALL} ddt ${OPTIONS}
 
-tar cfzv ${TARBALL} ${DIR}/* ${OPTIONS}
-
+popd
+mv ${TMPDIR}/${TARBALL} . 
 echo "Distfile created in '${TARBALL}'"
+
+#
+# Cleanup when we're done
+#
+rm -rf $TMPDIR
 
