823ca6898e7c15f1a68860b5d049390680a36179
doug.muth <doug.muth@de3767ec-2675-11df-8747-af52ddc49692>
2011-06-07 03:08:41 +0000

    Created placeholder for a settings page.
    git-svn-id: https://dougs-drupal-tools.googlecode.com/svn/trunk@31 de3767ec-2675-11df-8747-af52ddc49692

---
+++ menu.inc.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* This file contains our menu hook.
+* 
+* @author Douglas Muth <http://www.dmuth.org/>
+*
+*/
+
+
+/**
+* Our menu hook.
+*
+* @return array An array of menu items
+*/
+function ddt_menu() {
+
+	$retval = array();
+
+	$retval["admin/settings/ddt"] = array(
+		"title" => "DDT",
+		"description" => t("DDT Settings"),
+		"page callback" => "ddt_settings",
+		"page arguments" => array(3, 4, 5),
+		"access callback" => "user_access",
+		"access arguments" => array("administer nodes"),
+		);
+
+	return($retval);
+
+} // End of ddt_menu()
+
+
---
+++ settings.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Our main settings page.
+*
+* @author Douglas Muth <http://www.dmuth.org/>
+*
+*/
+
+
+/**
+* Our main settings page.
+*
+* @param string $arg, $arg2, $arg3 Optional page arguments
+*
+* @return string HTML code of the page
+*/
+function ddt_settings($arg = "", $arg2 = "", $arg3 = "") {
+
+	$retval = "";
+
+	return($retval);
+
+} // End of ddt_settings()
+
+
+
